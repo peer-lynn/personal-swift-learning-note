d515da1837d4383e3cf3721ec8a030fc16b0b867
jocelyn <shinekina@163.com>
2015-12-28 12:01:21 +0800

    unwrap

--- SwiftLearning-b3.playground/Contents.swift
+++ SwiftLearning-b3.playground/Contents.swift
@@ -152,9 +152,117 @@ if let result2 : Double? = divide(100, n2: 0){
 // 强制拆封
 // 如果我们能确定可选类型一定有值，那么在读取它的时候，可以在可选类型的后面加一个感叹号（！）来获取该值
 let result3 :Double? = divide(100, n2: 200)
-print(result3!)
+print(result3)
 
 // 隐式拆封, 在常量或者变量声明的时候，数据类型Double后面跟的事感叹号（！）而不是问号（？），在拆封的时候后面不加（！），这就是隐式拆封
 let result4: Double! = divide(100, n2: 200)
 print(result4)
+
+// 可选链
+// ？.
+//class Employee{
+//    var no : Int = 0
+//    var name : String = "Tony"
+//    var job: String?
+//    var salary: Double = 0
+//    var dept : Department = Department()
+//}
+//
+//class Department {
+//    var no : Int = 10
+//    var name : String = "SALES"
+//    var comp : Company = Company()
+//}
+//
+//class Company {
+//    var no : Int = 1000
+//    var name : String = "EOrient"
+//}
+//
+//var emp = Employee()
+//print(emp.dept.comp.name)
+
+// 想上面的这段代码，分别通过var dept : Department = Department()和 var comp : Company = Company() 对类进行了关联，形成了一个引用的链条， 但是这个“链条”的任何一个环节“断裂”（为nil）都无法进一步引用，更有可能的情况是，有一个环节可能为nil
+// 修改代码如下
+class Employee{
+    var no : Int = 0
+    var name : String = "Tony"
+    var job: String?
+    var salary: Double = 0
+    var dept : Department?
+}
+
+class Department {
+    var no : Int = 10
+    var name : String = "SALES"
+    var comp : Company?
+}
+
+class Company {
+    var no : Int = 1000
+    var name : String = "EOrient"
+}
+var emp = Employee()
+// 语句出错，这个引用方式不能应对可选类型
+//print(emp.dept.comp.name)？
+// 我们之前说过可选类型的应用可以用感叹号（！）进行强制拆封
+// 但是强制拆封时如果有一个环节为nil，会造成运行时错误
+//print(emp.dept!.comp!.name)
+// 用？代替！，这是种更加温柔的引用方式，问号表示引用的时候，如果某个环节为nul，他不会抛出错误，而是把nil值返回给引用者
+print(emp.dept?.comp?.name)
+// 用？号引用可选类型的方式就叫做可选链，他的引用目标还有方法，下标和嵌套类型等
+// 嵌套类型的例子
+class EmployeeNest{
+    var no : Int = 0
+    var name : String = "Tony"
+    var job: String?
+    var salary: Double = 0
+    var dept : DepartmentNest?
+    
+    struct DepartmentNest {
+        var no : Int = 10
+        var name : String = "SALES"
+    }
+    
+}
+var empNest = EmployeeNest()
+print(empNest.dept?.name)
+
+// 访问级别，访问修饰符
+// public internal private
+// 原则 : 统一性原则
+//
+
+// 练习
+enum ProductCategory: String{
+    case Washers = "washers", Dryers = "dryers", Toasters = "toasters"
+}
+var product = ProductCategory.Toasters
+if(product.rawValue == "toasters"){
+    print("烤箱")
+}
+if(product == .Toasters){
+    print("烤箱")
+}
+
+internal struct union{
+    var c : Character
+    var a : Int
+    var b : Double
+}
+
+var optionCod : String!
+if (optionCod != nil){
+    print("uppercase optionCod == \(optionCod.uppercaseString)")
+}else{
+    print("optionalCod is nil")
+}
+
+var optionCod1 : String?
+if (optionCod1 != nil){
+    print("uppercase optionCod == \(optionCod1?.uppercaseString)")
+}else{
+    print("optionalCod is nil")
+}
+
 
